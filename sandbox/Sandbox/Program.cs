02b9cbf55078feff11f1c11090d2617c53570614
JosephKemper <JosephKemper@gmail.com>
2023-01-02 10:44:32 -0700

    Testing demo code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
     }
 }

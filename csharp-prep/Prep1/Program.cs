b470efd3942e9ad54658c09cbc51e4c2c0a7fe03
JosephKemper <JosephKemper@gmail.com>
2023-01-02 12:31:36 -0700

    Syncing initial work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        // Get user first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Get user last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display name back to user in the following format
+        // "Your name is (last name), (First name) (last name).
+        Console.WriteLine ($"Your name is {lastName}, {firstName} {lastName}.")
     }
 }
 // Following git tutorial

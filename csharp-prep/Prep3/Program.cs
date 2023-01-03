bbece20843a051d2df35080bfb82fae81d4306d7
JosephKemper <JosephKemper@gmail.com>
2023-01-03 12:50:37 -0700

    Finished user input lines.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string numberInput = Console.ReadLine();
+        int magicNumber = int.Parse(numberInput);
+        Console.WriteLine(magicNumber);
+
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int numberGuess = int.Parse(guessInput);
+        Console.WriteLine(numberGuess);
     }
 }

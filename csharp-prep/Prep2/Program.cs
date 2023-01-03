be36fe3e599e065a77c558b5af02091e34de09c8
JosephKemper <JosephKemper@gmail.com>
2023-01-02 17:16:08 -0700

    Testing user grade entry.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        // Write a program to turn numbers into letter grade.
+
+        // Get grade from user.
+        Console.Write("Enter your percentage grade (leave off the % symbol): ");
+        // TODO: #1 Convert input to number format
+        int userScore = Console.ReadLine();
+        Console.WriteLine(userScore);
     }
 }

da6f1ac329b08aba38576e44276838c9038efff5
JosephKemper <JosephKemper@gmail.com>
2023-01-03 13:01:04 -0700

    Added comments to code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,22 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        // Get number from user
         Console.Write("What is the magic number? ");
         string numberInput = Console.ReadLine();
         int magicNumber = int.Parse(numberInput);
 
+        // Get guess from user
         Console.Write("What is your guess? ");
         string guessInput = Console.ReadLine();
         int numberGuess = int.Parse(guessInput);
         
+        // Check if guess is greater than number and return message
         if (numberGuess > magicNumber)
         {
             Console.WriteLine ("Lower");
         }
+        // Check if guess is less than number and return message
         else if (numberGuess < magicNumber)
         {
             Console.WriteLine ("Higher");
         }
+        // Return Equals message
         else
         {
             Console.WriteLine ("You Guessed it!");

0e5a429604953d0d378046377d83dfbd12ffade2
JosephKemper <JosephKemper@gmail.com>
2023-01-04 10:25:29 -0700

    Outlined core requirements for C# Prep 4 Overview

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        // Create a loop to collect numbers from user
+
+        // Add numbers to list
+
+        // Loop should continue until user enters 0
+
+        // Sum numbers in list
+
+        // Count the number of items in the list
+
+        // Calculate average of numbers in list
+
+        // Find largest number in list
     }
 }

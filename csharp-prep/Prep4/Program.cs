e4e6aca60afbc0a63eb6c683cc53262113efead0
JosephKemper <JosephKemper@gmail.com>
2023-01-04 12:54:29 -0700

    Added code to collect user input.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,9 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> userNumbers = new List<int>();
+
+        Console.WriteLine ("Enter a list of numbers, type 0 when finished.");
+
         // Create a loop to collect numbers from user
 
+        Console.Write ("Enter number: ");
+        string userInput = Console.ReadLine();
+
+        // Convert user input to number
+        int userNumber = int.Parse(userInput);
+
         // Add numbers to list
+        Console.WriteLine (userNumber);
 
         // Loop should continue until user enters 0
 

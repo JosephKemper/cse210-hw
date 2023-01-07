32201a0ab71ec0fad39c62748928cb093edda2d4
JosephKemper <JosephKemper@gmail.com>
2023-01-07 13:41:32 -0700

    Got code started for Job class.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    // store _company as string
+    public string _company;
+    //  store _jobTitle as string
+    public string _jobTitle;
+    // store _startYear as int
+    public int _startYear;
+    // store _endYear as int
+    public int _endYear;
+
+    
+}

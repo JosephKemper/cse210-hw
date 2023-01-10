83f4fa2e3789584146062349c3a0c7c9103e297a
JosephKemper <JosephKemper@gmail.com>
2023-01-10 08:03:33 -0700

    Instanciated 2 instances of class

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,11 +1,15 @@
+using System;
 public class Job
 {
     // store _company as string
     public string _company;
+
     //  store _jobTitle as string
     public string _jobTitle;
+
     // store _startYear as int
     public int _startYear;
+
     // store _endYear as int
     public int _endYear;
 
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Technical Support Project Supervisor";
+        job1._company = "NorthStar Home Security";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job ();
+        job2._jobTitle = "Virtual Support Specialist";
+        job2._company = "ADT";
+        job2._startYear = 2022;
+        job2._endYear = 2022;
+
     }
 }

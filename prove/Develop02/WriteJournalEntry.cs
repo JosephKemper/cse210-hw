1371c1dbcb0130e53e3a9664fc82dae56182ea55
JosephKemper <JosephKemper@gmail.com>
2023-01-23 19:21:55 -0700

    finished write journal class

--- prove/Develop02/WriteJournalEntry.cs
+++ prove/Develop02/WriteJournalEntry.cs
@@ -2,6 +2,22 @@ using System;
 
 public class WriteJournalEntry
 {
-    string currentDate = DateTime.UtcNow.ToString("dd MMM yyy");
+    public string Date ()
+    {
+        string currentDate = DateTime.UtcNow.ToString("dd-MMM-yyy");
+        return currentDate;
+    }
     
+    public string JournalEntry (string date, string prompt)
+    {
+        Console.WriteLine(prompt);
+        string currentEntry = Console.ReadLine();
+        List <string> combinedEntry = new List<string>{
+        date,
+        prompt,
+        currentEntry
+        };
+        return currentEntry;
+    }
+
 }

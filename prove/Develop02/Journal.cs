a89613cf2becf0e61d8399fa986908f58d494c04
JosephKemper <JosephKemper@gmail.com>
2023-01-28 12:10:20 -0700

    finished save and load methods

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,21 +1,20 @@
 using System;
+using System.IO;
 
 public class Journal
 {
     JournalPrompt newPrompt = new JournalPrompt ();
     List <JournalEntry> _completeJournal = new List <JournalEntry>();
-    
+    JournalEntry currentEntry = new JournalEntry();
+
     public void WriteJournal ()
     {
-        JournalEntry currentEntry = new JournalEntry();
-
-
         currentEntry._currentDate = DateTime.UtcNow.ToString("dddd, dd MMMM, yyyy");
         
         currentEntry._journalPrompt = newPrompt.GeneratePrompt();
         Console.WriteLine (currentEntry._journalPrompt);
 
-        currentEntry._entryText = Console.ReadLine();
+        currentEntry._journalText = Console.ReadLine();
 
         _completeJournal.Add (currentEntry);
 
@@ -26,6 +25,39 @@ public class Journal
         foreach (JournalEntry entry in _completeJournal)
         {
             entry.DisplayEntry();
+        }
+    }
+
+    public void SaveJournal()
+    {
+        Console.Write ("Please enter the file name: ");
+        string fileName = Console.ReadLine();
+        using (StreamWriter saveEntry = new StreamWriter (fileName))
+        {
+            foreach (JournalEntry entry in _completeJournal)
+            {
+                saveEntry.WriteLine ($"{entry._currentDate} , {entry._journalPrompt} , {entry._journalText}");
+            }
+        }
+    }
+
+    public void LoadJournal ()
+    {
+        Console.Write ("Enter the file name you wish to load: ");
+        string fileName = Console.ReadLine ();
+        string[] lines = System.IO.File.ReadAllLines(fileName);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split (" , ");
+            string entryDate = parts [0];
+            string entryPrompt = parts [1];
+            string entryText = parts [2];
+
+            currentEntry._currentDate = entryDate;
+            currentEntry._journalPrompt = entryPrompt;
+            currentEntry._journalText = entryText;
+
+            _completeJournal.Add(currentEntry);
         }
     }
 }
--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -3,13 +3,13 @@ using System;
 public class JournalEntry
 {
     public string _journalPrompt;
-    public string _entryText;
+    public string _journalText;
     public string _currentDate;
     public void DisplayEntry()
     {
         Console.WriteLine ($"Date: {_currentDate}");
         Console.WriteLine ($"Prompt {_journalPrompt}");
-        Console.WriteLine (_entryText);
+        Console.WriteLine (_journalText);
         Console.WriteLine();
         Console.WriteLine ("Press any key to continue ...");
         Console.WriteLine();

8e4e4cb05ea5e011e6881a092f010b462c9ab60f
JosephKemper <JosephKemper@gmail.com>
2023-01-27 18:41:59 -0700

    condensed code and implimented menu loop.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,15 +2,37 @@ using System;
 
 class Program
 {
+    Journal journal = new Journal();
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
-        Menu mainMenu = new Menu ();
-        mainMenu.DisplayMenu();
-        string userInput = mainMenu.GetUserChoice();
+        Console.WriteLine ("Welcome to the Journal Program!");
+        Console.WriteLine ("Please select one of the following choices:");
+        Console.WriteLine ("1. Write");
+        Console.WriteLine ("2. Display");
+        Console.WriteLine ("3. Load");
+        Console.WriteLine ("4. Save");
+        Console.WriteLine ("5. Quit");
+        Console.Write ("What would you like to do? ");
 
-        mainMenu.LoadUserChoice (userInput);
-        // Need a loop to continue running until 5 is selected
-        
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "1"){
+            // Write journal entry
+        }else if (userChoice == "2"){
+            // Display journals typed
+        }else if (userChoice == "3"){
+            // Load journals from file
+        }else if (userChoice == "4"){
+            // Save journals to file
+        }else if (userChoice == "5"){
+            // Exit program
+            Console.WriteLine ("Thank you for using our Journal Program!");
+            Console.WriteLine ("Come back soon.");
+            Environment.Exit(0);
+        }else {
+            // Catch incorrect entries to prevent app crashing
+            Console.WriteLine ("Please enter the number of the option you wish to select.");
+        }
+
     }
 }

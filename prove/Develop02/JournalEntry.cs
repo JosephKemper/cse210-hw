385793ec628bbd066fe37cbc1e2b9ad8864f1dc4
JosephKemper <JosephKemper@gmail.com>
2023-01-28 21:20:52 -0700

    Fixed spacing and character deletion issues

--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -11,14 +11,15 @@ public class JournalEntry
     // DisplayEntry class method teaches computer how to display a single entry to user
     public void DisplayEntry()
     {
-        Console.Write ("\b");
         Console.WriteLine ($"{_currentDate}");
         Console.WriteLine ($"{_journalPrompt}");
         Console.WriteLine (_journalText);
         Console.WriteLine();
         // Automatically pause after displaying each entry to allow the user a chance to read it
         Console.WriteLine ("Press any key to continue ...");
-        Console.Write ("\b");
         Console.ReadKey();
+        Console.Write ("\b");
+        Console.WriteLine ();
+        
     }
 }

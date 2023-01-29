1df36cc9da38e8d909df15035e8c9d482f4706c6
JosephKemper <JosephKemper@gmail.com>
2023-01-28 18:48:07 -0700

    added abstraction notes to JournalEntry class

--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -1,16 +1,21 @@
 using System;
-
+// Class JournalEntry to establish data type for journal storage
 public class JournalEntry
 {
+    // _journalPrompt member variable for storing the prompts for each journal entry
     public string _journalPrompt;
+    // _journalText member variable for storing the text of each journal entry
     public string _journalText;
+    // _currentDate member variable for storing the date each journal was written
     public string _currentDate;
+    // DisplayEntry class method teaches computer how to display the entry to user
     public void DisplayEntry()
     {
         Console.WriteLine ($"{_currentDate}");
         Console.WriteLine ($"Prompt {_journalPrompt}");
         Console.WriteLine (_journalText);
         Console.WriteLine();
+        // Automatically pause after displaying each entry to allow the user a chance to read it
         Console.WriteLine ("Press any key to continue ...");
         Console.WriteLine();
         Console.ReadKey();

7e2f24df2c463a23e9ab31123f243546b0856218
JosephKemper <JosephKemper@gmail.com>
2023-02-04 15:34:52 -0700

    basic program functionality written

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string userInput = "";
+        while (userInput != "quit"){
+        Console.Clear();
+        Console.WriteLine ("Doctrine and Covenants 6:34-36 Therefore, fear not, little flock; do good; let earth and hell combine against you, for if ye are built upon my rock, they cannot prevail. Behold, I do not condemn you; go your ways and sin no more; perform with soberness the work which I have commanded you. Look unto me in every thought; doubt not, fear not.");
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        userInput = Console.ReadLine ();
+        }
     }
 }
--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -7,9 +7,16 @@ public class Reference
 // Verse count, if 1 then it tells the class to use the single verse constructor, if multiple, then it tells the class to use the multiple verse constructor.
 
 // Book:String 
+private string _book;
+
 // Chapter:String
+private string _chapter;
+
 // FirstVerse:String
+private string _firstVerse;
+
 // LastVerse:String
+private string _lastVerse;
 
 // Look up -- Used by Scripture class to pull verse to memorize
 

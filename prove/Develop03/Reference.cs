42f6ac8c3012ba41ab67e855d298c740d7a47e12
JosephKemper <JosephKemper@gmail.com>
2023-02-11 13:36:08 -0700

    put GetLibrary method in Reference class

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -4,13 +4,23 @@ namespace Develop03;
 public class Reference
 {
     // Selects and returns scripture reference for user to memorize
+    private List<string> _referenceList = new List<string>();
+    private List<string> _textList = new List<string> ();
     private string _scriptureReference;
     private string _scriptureText;
-    private List<string> _scriptureList = new List<string> ();
     private int _listLength;
     private int _userSelection;
 
-    
+    public void GetLibrary ()
+    {
+         // Stretch Goal
+        // Load Scriptures from file
+        ScriptureLibrary scriptures = new ScriptureLibrary();
+        scriptures.LoadScriptures();
+        _referenceList = scriptures.ReturnReference();
+        _textList = scriptures.ReturnText();
+        _listLength = _referenceList.Count;
+    }
 
     public void SelectScripture (int userSelection = 0)
     {
@@ -18,6 +28,7 @@ public class Reference
         _userSelection = userSelection;
         
         // If userSelection = 0 generate random int to select scripture
+
 
         // Else subtract one from user selection to select scripture
 
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,48 +4,36 @@ namespace Develop03;
 public class Scripture
 {
     
-    /*
-    Keeps track of the reference and the text of the scripture. 
-    Can hide words and get the rendered display of the text.
-    */
-    private string _combinedReference;
-    string _verseText;
+    private List<string> _wordList = new List<string> ();
+    private string _currentReference;
+    private string _currentText;
+
+
+    
+    
     // method for converting array to list found https://www.c-sharpcorner.com/article/convert-an-array-to-a-list-in-c-sharp/
-    public void constructWordList ()
+    public void ConstructWordList ()
     {
-        string [] _wordArray = _verseText.Split(" ");
-        List <string> _wordList = new();
+        string [] _wordArray = _currentText.Split(" ");
         _wordList.AddRange(_wordArray);
-        
     }
         
          
     
     
-    // VisibleVerseList:List <String>
-
-    // Hide Words -- uses the hide function in the word class to hide each word as needed. 
     
-    // Get Rendered Text -- assembles the modified verse for display to user
-
-    // Set Rendered Text -- Displays rendered text to user
-    // Is Completely Hidden -- checks if the verse is completely hidden and exits the program after another input from user
-
-
-    // combine scripture -- combines the reference, and text of the scripture to one string
-    // display text -- display's the scripture text to the user.
 
     public string DisplayReference ()
     {
         Reference scriptureReference = new Reference();
             
-        _combinedReference = scriptureReference.ReturnReference();
-        return _combinedReference;
+        _currentReference = scriptureReference.ReturnReference();
+        return _currentReference;
      }
 
      public string DisplayVerse ()
      {
-        return _verseText;
+        return _currentText;
      }
 
     /*

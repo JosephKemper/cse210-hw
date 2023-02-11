8c24dd89367889d2ce64c1b77a822e9962ee68c8
JosephKemper <JosephKemper@gmail.com>
2023-02-11 10:12:41 -0700

    Outlined reference class

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,33 +2,39 @@ using System;
 
 public class Reference
 {
-// Keeps track of the book, chapter, and verse information.
-private string _combinedReference = "Doctrine and Covenants 6:34-36";
-// Verse count, if 1 then it tells the class to use the single verse constructor, if multiple, then it tells the class to use the multiple verse constructor.
+    // Selects and returns scripture reference for user to memorize
+    private string _scriptureReference = "Doctrine and Covenants 6:34-36";
+    private string _scriptureText;
+    private List<string> _scriptureList = new List<string> ();
+    private int _listLength;
+    private int _userSelection;
 
-// Book:String 
-private string _book;
+    public void LoadScriptures ()
+    {
+        // Stretch Goal loads scriptures from file into list
+    }
 
-// Chapter:String
-private string _chapter;
+    public void SelectScripture (int userSelection = 0)
+    {
+        // Stretch Goal allows user to select scripture or selects a random scripture
+        _userSelection = userSelection;
+        
+        // If userSelection = 0 generate random int to select scripture
 
-// FirstVerse:String
-private string _firstVerse;
+        // Else subtract one from user selection to select scripture
 
-// LastVerse:String
-private string _lastVerse;
+        // Assign scripture to verse and reference variables
 
-// Look up -- Used by Scripture class to pull verse to memorize
+    }
 
-// 1 verse constructor
-// Combines book name, chapter and verse into the following format Book c:v
-
-
-// multiple verse constructor
-// Combines book name, chapter and verse into the following format Book c:v-v1
 
     public string ReturnReference ()
     {
-        return _combinedReference;
+        return _scriptureReference;
+    }
+
+    public string ReturnText ()
+    {
+        return _scriptureText;
     }
 }

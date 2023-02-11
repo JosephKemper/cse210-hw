2bfbbe1e4805224c892373297188059feed89b7d
JosephKemper <JosephKemper@gmail.com>
2023-02-11 12:14:25 -0700

    Wrote ScriptureLibrary Class

--- prove/Develop03/ScriptureLibrary.cs
+++ prove/Develop03/ScriptureLibrary.cs
@@ -1,20 +1,38 @@
 using System;
-
+// learned about namespace from https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/classes
+namespace ScriptureProject;
 public class ScriptureLibrary
 {
     // Stretch Goal loads scriptures from file for use in program
-    private string scriptureFile = "Scripture Library.txt";
+    
     private List <string> _referenceList = new List <string> ();
     private List <string> _scriptureList = new List<string> ();
-    public void LoadScriptures ()
+    private string scriptureFile = "Scripture Library.txt";
+    
+    public void LoadScriptures()
     {
+        
         // Stretch Goal loads scriptures from file into list
         string[] lines = System.IO.File.ReadAllLines(scriptureFile);
         foreach (string line in lines)
         {
-            
+            string[] scriptureParts = line.Split("|~|");
+            string reference = scriptureParts [0];
+            string text = scriptureParts [1];
+
+            _scriptureList.Add(text);
+            _referenceList.Add(reference);
         }
     }
 
+    public List<string> ReturnReference()
+    {
+        return _referenceList;
+    }
+
+    public List<string> ReturnText()
+    {
+        return _scriptureList;
+    }
 
 }
--- prove/Develop03/ScriptureReference.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class ScriptureReference
-{
-    // Stretch Goal
-    // Class is used to teach computer how to read line in file
-    private string _reference;
-    private string _text;
-}

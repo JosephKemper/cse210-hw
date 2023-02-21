815ef8157388284d619ebe88829e72ce14e4606d
JosephKemper <JosephKemper@gmail.com>
2023-02-21 07:20:36 -0700

    wrote main program loop.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,7 +6,41 @@ namespace Develop04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Develop04 World!");
+            string _userSelection = "";
+            Console.WriteLine("Welcome to the Mindfulness Program");
+            while (_userSelection != "4")
+            {
+                Console.WriteLine("Menu Options");
+                Console.WriteLine("1. Start Breathing Activity");
+                Console.WriteLine("2. Start Reflecting Activity");
+                Console.WriteLine("3. Start Listing Activity");
+                Console.WriteLine("4. Quit");
+                Console.Write("Select a choice from the menu: ");
+                
+                _userSelection = Console.ReadLine();
+                Console.WriteLine();
+
+                if (_userSelection == "1")
+                {
+                    Console.WriteLine ("Run Breathing Activity");
+                }
+                else if (_userSelection == "2")
+                {
+                    Console.WriteLine ("Run Reflecting Activity");
+                }
+                else if (_userSelection == "3")
+                {
+                    Console.WriteLine ("Run Listing Activity");
+                }
+                else if (_userSelection == "4")
+                {
+                    Console.WriteLine ("Come Back soon!");
+                }
+                else
+                {
+                    Console.WriteLine ("Please enter the number of the activity you wish to run.");
+                }
+            }
         }
     }
 }

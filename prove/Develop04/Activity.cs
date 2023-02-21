2fd35377cb9b7a77a159e97333e8e2dc6a77c1a9
JosephKemper <JosephKemper@gmail.com>
2023-02-21 07:37:14 -0700

    wrote core of DisplayIntroMessage Method.

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Activity
+{
+    private string _getSeconds;
+    private string _intSeconds;
+
+    public void DisplayIntroMessage ()
+    {
+        Console.WriteLine ("Sample intro message");
+    }
+}

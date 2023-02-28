0d5a4ff03b61b4743f8393e487fc72b02b674eba
JosephKemper <JosephKemper@gmail.com>
2023-02-27 20:40:39 -0700

    Wrote code to add shapes to and iterate a list

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -8,5 +8,31 @@ class Program
         Square testSquare = new Square("Brown", 4);
         Console.WriteLine (testSquare.GetColor());
         Console.WriteLine (testSquare.GetArea());
-    }
+
+        Console.WriteLine ();
+
+        Rectangle testRectangle = new Rectangle ("Blue", 4, 5);
+        Console.WriteLine (testRectangle.GetColor());
+        Console.WriteLine (testRectangle.GetArea());
+
+        Console.WriteLine ();
+
+        Circle testCircle = new Circle ("Green", 7);
+        Console.WriteLine (testCircle.GetColor());
+        Console.WriteLine (testCircle.GetArea());
+
+        Console.WriteLine ();
+
+        List<Shape> shapesList = new List<Shape> ();
+        shapesList.Add (testCircle);
+        shapesList.Add (testRectangle);
+        shapesList.Add (testSquare);
+
+        foreach (Shape shape in shapesList)
+        {
+            Console.WriteLine (shape.GetColor());
+            Console.WriteLine (shape.GetArea());
+        }
+        }
+
 }

6caa963540bfaa444527da44f3fb9e93de593542
JosephKemper <JosephKemper@gmail.com>
2023-02-27 19:58:30 -0700

    Confirmed Square class worked properly

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Square testSquare = new Square("Brown", 4);
+        Console.WriteLine (testSquare.GetColor());
+        Console.WriteLine (testSquare.GetArea());
     }
 }

e123094bc23b078f50114954ea38ffa7edd698f8
JosephKemper <JosephKemper@gmail.com>
2023-02-27 18:52:03 -0700

    Wrote Square class, and fixed shape

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -3,6 +3,11 @@ using System;
 public abstract class Shape
 {
     private string _color;
+
+    public Shape (string color)
+    {
+        _color = color;
+    }
     
     public string GetColor()
     {

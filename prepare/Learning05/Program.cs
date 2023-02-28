6aebc8c722b22661493c70b58412d2d3db4f4d11
JosephKemper <JosephKemper@gmail.com>
2023-02-27 21:03:37 -0700

    commented out test code

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,25 +6,25 @@ class Program
     {
         Console.WriteLine("Hello Learning05 World!");
         Square testSquare = new Square("Brown","Square", 4);
-        Console.WriteLine (testSquare.GetColor());
-        Console.WriteLine (testSquare.GetArea());
-        Console.WriteLine (testSquare.GetName());
+        // Console.WriteLine (testSquare.GetColor());
+        // Console.WriteLine (testSquare.GetArea());
+        // Console.WriteLine (testSquare.GetName());
 
-        Console.WriteLine ();
+        
 
         Rectangle testRectangle = new Rectangle ("Blue","Rectangle", 4, 5);
-        Console.WriteLine (testRectangle.GetColor());
-        Console.WriteLine (testRectangle.GetArea());
-        Console.WriteLine (testRectangle.GetName());
+        // Console.WriteLine (testRectangle.GetColor());
+        // Console.WriteLine (testRectangle.GetArea());
+        // Console.WriteLine (testRectangle.GetName());
 
-        Console.WriteLine ();
+        
 
         Circle testCircle = new Circle ("Green","Circle", 7);
-        Console.WriteLine (testCircle.GetColor());
-        Console.WriteLine (testCircle.GetArea());
-        Console.WriteLine (testCircle.GetName());
+        // Console.WriteLine (testCircle.GetColor());
+        // Console.WriteLine (testCircle.GetArea());
+        // Console.WriteLine (testCircle.GetName());
 
-        Console.WriteLine ();
+        
 
         List<Shape> shapesList = new List<Shape> ();
         shapesList.Add (testCircle);

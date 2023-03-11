026f230e9f6579669683ed17f9cc530ccc81430c
JosephKemper <JosephKemper@gmail.com>
2023-03-11 16:56:11 -0700

    Issue #17 is caused by multiple instances of _goalList being created, Refactoring code

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        int _userChoice = 0;
-
-        while (_userChoice != 6)
-        {
-        Menu runMenu = new Menu ();
-        _userChoice = runMenu.GetMainMenuChoice();
-        }
+        Console.WriteLine ("Hello Develop 5 world;");
     }
 }

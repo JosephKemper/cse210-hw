404a15a9459e19074060c4942abe73b8e60f14b4
JosephKemper <JosephKemper@gmail.com>
2023-03-20 20:42:13 -0600

    started writing code to build first the Abstraction assignment.

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,52 @@
+using System;
+
+class Video
+{
+private string _title;
+private string _author;
+private string _fileName = "RawVideoFile.txt";
+protected List <Comment> _comments = new List<Comment>();
+protected List <string> _rawData = new List<string> ();
+
+public void LoadFile ()
+{
+    
+}
+
+
+public void ProcessComments ()
+{
+}
+
+public void DisplayVideoData ()
+{
+}
+
+public void GetTitle ()
+{
+}
+
+public void SetTitle ()
+{
+}
+
+public void GetAuthor ()
+{
+}
+
+public void SetAuthor ()
+{
+}
+
+public void GetLength ()
+{
+}
+
+public void SetLength ()
+{
+}
+
+public Video ()
+{
+}
+}

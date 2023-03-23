46c535af670a30c0a4dc615be8a4674fc77870dc
JosephKemper <JosephKemper@gmail.com>
2023-03-22 19:38:31 -0600

    Filled in basic parts of Comment

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,4 +2,21 @@ using System;
 
 class Comment
 {
+    private string _name;
+    private string _comment;
+
+    public void DisplayComment (){
+    }
+
+    public void GetName (){
+    }
+    
+    public void SetName (){
+    }
+
+    public void GetComment (){
+    }
+
+    public void SetComment (){
+    }
 }

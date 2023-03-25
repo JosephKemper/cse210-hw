11413e6f26ba1b82f0eea122e2ac89180565cade
JosephKemper <JosephKemper@gmail.com>
2023-03-25 13:37:46 -0600

    Wrote code to load customer order

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,10 +1,15 @@
 using System;
 
 class Order{
+    // Names and addresses found in CustomerOrders.txt were made up by Bing's AI
+    private string _customerOrders = "CustomerOrders.txt";
     private string _rawOrders;
     private List<string> _ordersList = new List<string>();
 
-    public void LoadOrders (string filename){
+    public void LoadOrders (){
+        foreach (string line in File.ReadLines(_customerOrders)){
+            _ordersList.Add(line);
+            }
         }
     
     public void PrepOrders(string rawOrders){
@@ -12,6 +17,8 @@ class Order{
     
     public void DisplayOrders (List<string>ordersList){
         }
+    public Order (){
+    }
     public Order(string filename){
         }
     }

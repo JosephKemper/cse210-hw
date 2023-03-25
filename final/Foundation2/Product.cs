db111dfcfc8afd34acf77ccd272d0ff5fb83bc79
JosephKemper <JosephKemper@gmail.com>
2023-03-25 12:30:33 -0600

    built product/price list and wrote LoadProducts method

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -4,7 +4,19 @@ public class Product {
     private double _productPrice;
     private int _quantity;
     private string _productID;
+    private string _productFileName = "ProductPriceList.txt";
+    private Dictionary<string, double> _productDictionary = new Dictionary<string, double>();
 
+    public void LoadProducts (){
+        foreach (string line in File.ReadLines(_productFileName)){
+            string [] parts = line.Split("|");
+
+            _productID = parts[0];
+            _productPrice = double.Parse(parts[1]);
+
+            _productDictionary.Add(_productID,_productPrice);
+            }
+        }
     public double FindPrice (string productID){
         return _productPrice;
         }

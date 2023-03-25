6324c9043364f7a52238d24c69496451aa836c5c
JosephKemper <JosephKemper@gmail.com>
2023-03-25 10:52:15 -0600

    Got Product Class Outline populated

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,1 +1,21 @@
 using System;
+
+public class Product {
+    private double _productPrice;
+    private int _quantity;
+    private string _productID;
+
+    public double FindPrice (string productID){
+        return _productPrice;
+        }
+    
+    public double CalculateSubtotal(double productPrice, int quantity){
+        return productPrice * quantity;
+        }
+    
+    public Product(){
+        }
+
+    public Product (string productID, double productPrice, int quantity){
+        }
+    }

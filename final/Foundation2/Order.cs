569d75c42f8b186579e5446b2ce282e97e37d52f
JosephKemper <JosephKemper@gmail.com>
2023-03-25 10:27:19 -0600

    Started getting Project 2 set up.

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Order{
+    private string _rawOrders;
+    private List<string> _ordersList = new List<string>();
+
+    public void LoadOrders (string filename){
+        }
+    
+    public void PrepOrders(string rawOrders){
+        }
+    
+    public void DisplayOrders (List<string>ordersList){
+        }
+    public Order(string filename){
+        }
+    }

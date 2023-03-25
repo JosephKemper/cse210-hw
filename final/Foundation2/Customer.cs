ec1100e93b26801d5507316c60be1c75cbd7cb8d
JosephKemper <JosephKemper@gmail.com>
2023-03-25 10:37:23 -0600

    Got Customer Class Ouline populated

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,1 +1,24 @@
 using System;
+
+public class Customer{
+    private string _customerName;
+    private string _unformattedAddress;
+    private Address _customerAddress;
+
+    public void ProcessCustomerInfo (string _unformattedCustomerInfo){
+        }
+    
+    public void FormatAddress (string unformattedAddress){
+        }
+    
+    public string ReturnCustomerName(){
+        return _customerName;
+        }
+    public Address ReturnAddress(){
+        return _customerAddress;
+        }
+    
+    public Customer (string unformattedCustomerInfo){
+        }
+
+    }

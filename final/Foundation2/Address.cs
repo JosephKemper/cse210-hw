09ee02ce467b763da3e95c72bacf840dad460f58
JosephKemper <JosephKemper@gmail.com>
2023-03-25 10:43:14 -0600

    Got Address Class outline populated

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -1,4 +1,24 @@
 using System;
 
 public class Address{
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _zip;
+    private string _country;
+
+    public void ProcessAddressInfo(string unformattedAddress){
+        }
+    
+    public void CheckCountry (){
+        }
+    
+    public void ReturnFormattedAddress(){
+        }
+    
+    public Address (){
+    }
+
+    public Address (string unformattedAddress){
+        }
     }

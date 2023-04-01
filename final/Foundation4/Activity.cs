0c088551ec04c3f2291c67b220c786d80ef0da70
JosephKemper <JosephKemper@gmail.com>
2023-03-31 18:19:49 -0600

    Fixed rounding errors

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -34,11 +34,11 @@ public abstract class Activity{
     public abstract double CalculateSpeed(double minutes, double stat);
     public abstract double CalculatePace(double minutes, double stat);
     public void GetDate (){
-        _date = DateTime.Now.ToString("dddd, dd MMMM, yyyy");
+        _date = DateTime.Now.ToString("dd MMM yyyy");
         }
     public void CreateSummary (double minutes, double stat){
         //03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
-        Console.WriteLine($"{_date}, {_activity} ({_minutes} min)- Distance {CalculateDistance(minutes, stat)}, Speed {CalculateSpeed(minutes, stat)} mph, Pace: {CalculatePace(minutes, stat)}");
+        Console.WriteLine($"{_date}, {_activity} ({_minutes} min)- Distance {Math.Round(CalculateDistance(minutes, stat),2)}, Speed {Math.Round(CalculateSpeed(minutes, stat),2)} mph, Pace: {Math.Round(CalculatePace(minutes, stat),2)} min per mile");
         }
     public Activity(string activityInfo){
         LoadActivity(activityInfo);
